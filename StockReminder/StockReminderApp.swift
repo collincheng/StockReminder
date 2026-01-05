b760e858340a34bada01ca0bcb332f6d5c473cd5
Colin Cheng <49746060+collincheng@users.noreply.github.com>
2026-01-05 11:45:06 +0800

    Initial Commit

---
+++ StockReminder/StockReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockReminderApp.swift
+//  StockReminder
+//
+//  Created by Colin on 2026/1/5.
+//
+
+import SwiftUI
+
+@main
+struct StockReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
